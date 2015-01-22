ebed999202fd8acb090b1efcc13d318a689e7429
nongxiaoming <nongxiaoming@gmail.com>
2015-01-22 14:32:13 +0800

    add the eeprom driver.

---
+++ software/drivers/drv_eeprom.c
@@ -0,0 +1,32 @@
+#include "drv_eeprom.h"
+#include "drv_iap.h"
+
+/* Write data to EEPROM */
+uint8_t EEPROM_Write(uint32_t dstAdd, uint8_t *ptr, uint32_t byteswrt)
+{
+	uint32_t command[5], result[4];
+
+	command[0] = IAP_EEPROM_WRITE;
+	command[1] = dstAdd;
+	command[2] = (uint32_t) ptr;
+	command[3] = byteswrt;
+	command[4] = SystemCoreClock / 1000;
+	iap_entry(command, result);
+
+	return result[0];
+}
+
+/* Read data from EEPROM */
+uint8_t EEPROM_Read(uint32_t srcAdd, uint8_t *ptr, uint32_t bytesrd)
+{
+	uint32_t command[5], result[4];
+
+	command[0] = IAP_EEPROM_READ;
+	command[1] = srcAdd;
+	command[2] = (uint32_t) ptr;
+	command[3] = bytesrd;
+	command[4] = SystemCoreClock / 1000;
+	iap_entry(command, result);
+
+	return result[0];
+}
---
+++ software/drivers/drv_eeprom.h
@@ -0,0 +1,34 @@
+#ifndef __DRV_EEPROM_H_
+#define __DRV_EEPROM_H_
+#include <lpc15xx.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/**
+ * @brief	Write data to EEPROM
+ * @param	address		: EEPROM address to be written to
+ * @param	ptr			: Pointer to buffer to write from
+ * @param	len	: Number of bytes to write to EEPROM
+ * @return	An IAP response definition from drv_iap.h
+ */
+uint8_t EEPROM_Write(uint32_t address, uint8_t *data, uint32_t len);
+
+/**
+ * @brief	Read data from EEPROM
+ * @param	address	: EEPROM address to be read from
+ * @param	data		: Pointer to buffer to read to
+ * @param	len	: Number of bytes to read from EEPROM
+ * @return	An IAP response definition from drv_iap.h
+ */
+uint8_t EEPROM_Read(uint32_t address, uint8_t *data, uint32_t len);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __EEPROM_H_ */
