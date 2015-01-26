af6fc1366a201290178f9cf7a7bd1e92ca69c4d9
nongxiaoming <nongxiaoming@gmail.com>
2015-01-26 11:19:51 +0800

    add the Firmware.

---
+++ software/Firmware/applications/application.c
@@ -0,0 +1,65 @@
+/*
+ * File      : application.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2009-01-05     Bernard      the first version
+ */
+
+#include <rtthread.h>
+#include "drv_led.h"
+#ifdef RT_USING_FINSH
+#include <finsh.h>
+#include <shell.h>
+#endif
+
+void rt_init_thread_entry(void* parameter)
+{
+#ifdef RT_USING_FINSH
+	/* initialization finsh shell Component */
+    finsh_system_init();
+	  finsh_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
+}
+
+void rt_led_thread_entry(void* parameter)
+{
+ uint16_t count = 0;
+ led_hw_init();
+ while(1)
+   {
+		 count ++;
+		 if((count%3)==0)
+			 led_hw_rever(LED_BLUE);
+		 if((count%5)==0)
+			 led_hw_rever(LED_GREEN);
+		 if((count%7)==0)
+			 led_hw_rever(LED_RED);
+		 
+		 rt_thread_delay(RT_TICK_PER_SECOND/10);
+	 }
+}
+
+int rt_application_init()
+{
+    rt_thread_t tid;
+
+    tid = rt_thread_create("init",
+        rt_init_thread_entry, RT_NULL,
+        2048, RT_THREAD_PRIORITY_MAX/3, 20);
+    if (tid != RT_NULL)
+        rt_thread_startup(tid);
+  
+	  tid = rt_thread_create("led",
+        rt_led_thread_entry, RT_NULL,
+        512, RT_THREAD_PRIORITY_MAX-3, 5);
+    if (tid != RT_NULL)
+        rt_thread_startup(tid);
+    return 0;
+}
