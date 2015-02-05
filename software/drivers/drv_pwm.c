363c32342e51623c55ddaeb4692a596e9b6fae29
xiaonong <nongxiaoming@gmail.com>
2015-02-05 21:44:40 +0800

    add the pwm driver.

---
+++ software/drivers/drv_pwm.c
@@ -0,0 +1,13 @@
+#include "drv_pwm.h"
+
+
+
+rt_err_t pwm_hw_init(void)
+{
+
+
+    return RT_EOK;
+}
+
+INIT_DEVICE_EXPORT(pwm_hw_init);
+
---
+++ software/drivers/drv_pwm.h
@@ -0,0 +1,8 @@
+#ifndef __DRV_PWM_H
+#include <lpc15xx.h>
+#include <rtthread.h>
+
+
+rt_err_t pwm_hw_init(void);
+
+#endif
